1ff701772b0ef44d58d826f341652552671242ba
isacarvid <70700849+isacarvid@users.noreply.github.com>
2021-02-10 09:17:14 +0100

    Issue/5 parsing webhook (#7)
    * Created function for sending email and function to write body and corresponding tests
    
    * testing webhook
    
    * testing webhook
    
    * testing webhook
    
    * testing webhook
    
    * testing webhook
    
    * testing webhook
    
    * testing webhook
    
    * testing webhook
    
    * testing webhook
    
    * testing webhook
    
    * issue #5 adding parsing to json
    
    Co-authored-by: Eva Despinoy <despinoy@kth.se>
    Co-authored-by: Isac Arvidsson <isacarvidsson@mbpsomtlhorisac.mynet>

--- CIServer/src/main/java/CIServer/CIServer.java
+++ CIServer/src/main/java/CIServer/CIServer.java
@@ -1,7 +1,11 @@
 package CIServer;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.ServletException;
+import java.lang.Object;
+
+import java.util.Properties;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -9,75 +13,74 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.http.HttpMethod;
 import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.handler.AbstractHandler;
+import org.json.JSONObject;
 
-
-
+import models.WebhookRequest;
 
 /**
- Skeleton of a ContinuousIntegrationServer which acts as webhook
- See the Jetty documentation for API documentation of those classes.
-*/
+ * Skeleton of a ContinuousIntegrationServer which acts as webhook See the Jetty
+ * documentation for API documentation of those classes.
+ */
 
-public class CIServer extends AbstractHandler
-{
+public class CIServer extends AbstractHandler {
+	// used to start the CI server in command line
+	public static void main(String[] args) throws Exception {
+		Server server = new Server(8095);
+		server.setHandler(new CIServer());
+		server.start();
+		server.join();
+	}
 
-    // used to start the CI server in command line
-    public static void main(String[] args) throws Exception
-    {
-        Server server = new Server(8095);
-        server.setHandler(new CIServer());
-        server.start();
-        server.join();
-    }
-
-        
+	
 	@Override
 	public void handle(String target, Request baseRequest, jakarta.servlet.http.HttpServletRequest request,
 			jakarta.servlet.http.HttpServletResponse response) throws IOException, jakarta.servlet.ServletException {
-        response.setContentType("text/html;charset=utf-8");
-        response.setStatus(HttpServletResponse.SC_OK);
-        baseRequest.setHandled(true);
-        
-        String body = getBody(request);
-        System.out.println(body);
-        //body = parseJSON(body);
-        System.out.println( "\n" + baseRequest + "\n" + response);
-
-        // here you do all the continuous integration tasks
-        // for example
-        // 1st clone your repository
-        // 2nd compile the code
-        response.getWriter().println("CI job done");
+		response.setContentType("text/html;charset=utf-8");
+		response.setStatus(HttpServletResponse.SC_OK);
+		baseRequest.setHandled(true);
 		
+		if (request.getMethod() == "POST") {
+			String body = getBody(request);
+			try {
+				WebhookRequest webhookRequest = new WebhookRequest(new JSONObject(body));
+			} catch (Exception e) {
+				e.printStackTrace();
+			} 
+		}
+		
+		// body = parseJSON(body);
+		System.out.println("\n" + baseRequest + "\n" + response);
+
+		// here you do all the continuous integration tasks
+		// for example
+		// 1st clone your repository
+		// 2nd compile the code
+		response.getWriter().println("CI job done");
 	}
 
-
-	private String parseJSON(String body) {
-		return null;
-	}
 
 	private String getBody(jakarta.servlet.http.HttpServletRequest request) throws IOException {
 		String body;
 		StringBuilder stringBuilder = new StringBuilder();
-        BufferedReader bufferedReader = null;
-        InputStream inputStream = request.getInputStream();
-        if (inputStream != null) {
-            bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
-            char[] charBuffer = new char[128];
-            int bytesRead = -1;
-            while ((bytesRead = bufferedReader.read(charBuffer)) > 0) {
-                stringBuilder.append(charBuffer, 0, bytesRead);
-            }
-        } else {
-            stringBuilder.append("");
-        }
-        bufferedReader.close();
-        body = stringBuilder.toString();
-        
-        return body;
-	}
+		BufferedReader bufferedReader = null;
+		InputStream inputStream = request.getInputStream();
+		if (inputStream != null) {
+			bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+			char[] charBuffer = new char[128];
+			int bytesRead = -1;
+			while ((bytesRead = bufferedReader.read(charBuffer)) > 0) {
+				stringBuilder.append(charBuffer, 0, bytesRead);
+			}
+		} else {
+			stringBuilder.append("");
+		}
+		bufferedReader.close();
+		body = stringBuilder.toString();
 
+		return body;
+	}
 
 }
