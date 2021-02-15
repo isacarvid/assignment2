0a171be8c300f683a745a09789d4bf815d1c1956
isacarvid <isac.arvidsson97@gmail.com>
2021-02-15 14:29:37 +0100

    adding server port as input argument (#31)
    Co-authored-by: Isac Arvidsson <isacarvidsson@MacBook-Pro-som-tillhor-Isac.local>

--- CIServer/src/main/java/CIServer/Main.java
+++ CIServer/src/main/java/CIServer/Main.java
@@ -1,9 +1,9 @@
 package CIServer;
-
+ import java.lang.String;
 public class Main {
 
 	public static void main(String[] args) throws Exception {
 		CIServer server = new CIServer();
-		server.startServer();
+		server.startServer(Integer.parseInt(args[0]));
 	}
 }
